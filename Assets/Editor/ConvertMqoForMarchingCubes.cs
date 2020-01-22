2fdf0f6f4d10c64c2e4058de28f4ccf7400bf5db
abarabone <abara00@live.jp>
2020-01-23 02:18:09 +0900

    a

--- Assets/Editor/ConvertMqoForMarchingCubes.cs
+++ Assets/Editor/ConvertMqoForMarchingCubes.cs
@@ -1,59 +1,100 @@
-﻿//using System.Collections;
-//using System.Collections.Generic;
-//using System.IO;
-//using System.Linq;
-//using UnityEngine;
-//using UnityEditor;
-//using Sprache;
+﻿using System.Collections;
+using System.Collections.Generic;
+using System;
+using System.IO;
+using System.Linq;
+using UnityEngine;
+using UnityEditor;
+using Sprache;
 
-//public class ConvertMqoForMarchingCubes : MonoBehaviour
-//{
-//    [MenuItem( "Assets/Convert Mqo For Marching Cubes" )]
-//    static public void Create()
-//    {
-//        if( Selection.objects == null ) return;
+public class ConvertMqoForMarchingCubes : MonoBehaviour
+{
+    [MenuItem( "Assets/Convert Mqo For Marching Cubes" )]
+    static public void Create()
+    {
+        if( Selection.objects == null ) return;
 
-//        var qMqo =
-//        Selection.objects
-//            .Select( o => AssetDatabase.GetAssetPath( o ) )
-//            .Where( path => Path.GetExtension( path ) == ".mqo" )
-//            .Select( path => new StreamReader(path) )
-//            ;
-//        foreach( var f in qMqo )
-//        {
-//            using( f )
-//            {
-//                var s = f.ReadToEnd();
-//                convertToMesh_( s );
-//            }
-//        }
+        var qMqo =
+        Selection.objects
+            .Select( o => AssetDatabase.GetAssetPath( o ) )
+            .Where( path => Path.GetExtension( path ) == ".mqo" )
+            .Select( path => new StreamReader( path ) )
+            ;
+        foreach( var f in qMqo )
+        {
+            using( f )
+            {
+                var s = f.ReadToEnd();
+                convertToMesh_( s );
+            }
+        }
 
-//        return;
+        return;
 
 
-//        void convertToMesh_( string s )
-//        {
-//            var qObjectName =
-//                from obj in Parse.String( "Object" )
-//                from ws0 in Parse.WhiteSpace.Many()
-//                from d0 in Parse.Char( '"' )
-//                from obj_name in Parse.AnyChar.Many().Text()
-//                from d1 in Parse.Char( '"' )
-//                from ws1 in Parse.WhiteSpace.Many()
-//                from blacket in Parse.Char( '{' )
-//                select obj_name
-//                ;
-//            var qObject =
-//                from obj_name in qObjectName
-//                from content in Parse.AnyChar.Many().Text()
-//                select (obj_name, content)
-//                ;
-//            var q =
-//                from content in qObject.DelimitedBy( qObjectName )
-//                select content
-//                ;
-//            var txt = q.Parse( s );
-//            Debug.Log( txt.First().obj_name + " " + txt.First().content );
-//        }
-//    }
-//}
+        void convertToMesh_( string s )
+        {
+
+            var qNotQuote =
+                from text in Parse.AnyChar.Except( Parse.Char( '"' ) ).Many().Text()
+                select text
+                ;
+            Func<string, Parser<string>> qSkipUntil = target =>
+                from _ in Parse.AnyChar.Except( Parse.String( target ) ).Many().Text()
+                select _
+                ;
+
+            Func<string, Parser<string>> qTagName = tag =>
+                from _ in qSkipUntil( tag )
+                from obj in Parse.String( tag )
+                from tag_name in qNotQuote.Contained( Parse.Char( '"' ), Parse.Char( '"' ) ).Token()
+                select tag_name
+                ;
+            Func<string, Parser<string>> qTagContent = tag =>
+                from _ in qSkipUntil( tag )
+                from name in Parse.String( tag )
+                from __ in qSkipUntil( "{" )
+                from content in Parse.CharExcept( '}' ).Many().Contained( Parse.Char( '{' ), Parse.Char( '}' ) ).Text()
+                select content
+                ;
+            var qObjectContent =
+                from _st in qSkipUntil("{")
+                from vtx in qTagContent("vertex" )
+                from face in qTagContent( "face" )
+                from _ed in qSkipUntil( "}" )
+                select (vtx, face)
+                ;
+            var qObject =
+                from _ in qSkipUntil("Object")
+                from obj_name in qTagName( "Object" )
+                from content in qObjectContent
+                select (obj_name, content.vtx, content.face)
+                ;
+            var qAllObjects =
+                from objects in qObject.Many()
+                select objects
+                ;
+            var objectTexts = qAllObjects.Parse( s );
+
+            var qVtx =
+                from v0 in Parse.Number.Token()
+                from v1 in Parse.Number.Token()
+                from v2 in Parse.Number.Token()
+                select (v0: int.Parse( v0 ), v1: int.Parse( v1 ), v2: int.Parse( v2 ))
+                ;
+            var qTri =
+                from corner_count in Parse.Numeric.Token()
+
+                let faceText = corner_count == 3
+                    ? Parse.Numeric.Or( Parse.WhiteSpace ).Many().Contained( Parse.String( "V(" ), Parse.Char(')') ).Text()
+                    : Parse.Return("")
+                from f0 in 
+                select faceText
+
+            foreach( var txt in objectTexts )
+            {
+                Debug.Log( $"{txt.obj_name} {txt.vtx} {txt.face}" );
+            }
+        }
+    }
+}
