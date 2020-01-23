d4cb62ac4a80b1f1f0b406ea2a287c962d23e2ad
abarabone <abara00@live.jp>
2020-01-23 22:21:47 +0900

    convert mqo ok

--- Assets/Editor/ConvertMqoForMarchingCubes.cs
+++ Assets/Editor/ConvertMqoForMarchingCubes.cs
@@ -14,25 +14,31 @@ public class ConvertMqoForMarchingCubes : MonoBehaviour
     {
         if( Selection.objects == null ) return;
 
-        var qMqo =
-        Selection.objects
+        var qMqoPath = Selection.objects
             .Select( o => AssetDatabase.GetAssetPath( o ) )
             .Where( path => Path.GetExtension( path ) == ".mqo" )
-            .Select( path => new StreamReader( path ) )
             ;
-        foreach( var f in qMqo )
+        foreach( var path in qMqoPath )
         {
-            using( f )
+            using( var f = new StreamReader(path) )
             {
                 var s = f.ReadToEnd();
-                convertToMesh_( s );
+                var meshes = convertToMeshes_( s );
+
+                foreach( var m in meshes )
+                {
+                    var folderPath = Path.GetDirectoryName( path );
+                    AssetDatabase.CreateAsset( m, $"{folderPath}/{m.name}.asset" );
+                }
+
+                AssetDatabase.Refresh();
             }
         }
 
         return;
 
 
-        void convertToMesh_( string s )
+        Mesh[] convertToMeshes_( string s )
         {
 
             var qNotQuote =
@@ -58,14 +64,14 @@ public class ConvertMqoForMarchingCubes : MonoBehaviour
                 select content
                 ;
             var qObjectContent =
-                from _st in qSkipUntil("{")
-                from vtx in qTagContent("vertex" )
+                from _st in qSkipUntil( "{" )
+                from vtx in qTagContent( "vertex" )
                 from face in qTagContent( "face" )
                 from _ed in qSkipUntil( "}" )
                 select (vtx, face)
                 ;
             var qObject =
-                from _ in qSkipUntil("Object")
+                from _ in qSkipUntil( "Object" )
                 from obj_name in qTagName( "Object" )
                 from content in qObjectContent
                 select (obj_name, content.vtx, content.face)
@@ -76,24 +82,51 @@ public class ConvertMqoForMarchingCubes : MonoBehaviour
                 ;
             var objectTexts = qAllObjects.Parse( s );
 
-            var qVtx =
-                from v0 in Parse.Number.Token()
-                from v1 in Parse.Number.Token()
-                from v2 in Parse.Number.Token()
-                select (v0: int.Parse( v0 ), v1: int.Parse( v1 ), v2: int.Parse( v2 ))
+
+            var qExponent =
+                from _ in Parse.Char( 'E' )
+                from sign in Parse.Chars( "+-" )
+                from num in Parse.Number
+                select $"E{sign}{num}"//String.Format( "E{0}{1}", sign, num )
                 ;
-            var qTri =
-                from corner_count in Parse.Numeric.Token()
+            var qFloatEx =
+                from negative in Parse.Char( '-' ).Optional().Select( x => x.IsDefined ? x.Get().ToString() : "" )
+                from num in Parse.Decimal
+                from e in qExponent.Optional().Select( x => x.IsDefined ? x.Get() : "" )
+                select Convert.ToSingle( negative + num + e )
+                ;
+            var qVtx =
+                from v0 in qFloatEx.Token()
+                from v1 in qFloatEx.Token()
+                from v2 in qFloatEx.Token()
+                select new []{v0, v1, v2}
+                ;
+            var qIdx =
+                from corner_length in Parse.Decimal.Token().Select( x => int.Parse( x ) )
+                from index_body in Parse.Numeric.Or( Parse.WhiteSpace ).Many().Contained( Parse.String( "V(" ), Parse.Char( ')' ) ).Token().Text()
+                from _ in Parse.AnyChar.Until( Parse.LineEnd )
+                select (corner_length, indices:index_body.Split(' ').Select(x => int.Parse(x)).ToArray() )
+                ;
 
-                let faceText = corner_count == 3
-                    ? Parse.Numeric.Or( Parse.WhiteSpace ).Many().Contained( Parse.String( "V(" ), Parse.Char(')') ).Text()
-                    : Parse.Return("")
-                from f0 in 
-                select faceText
+            return objectTexts
+                .Select( x => createMesh_( x.obj_name, x.vtx, x.face ) )
+                .ToArray();
 
-            foreach( var txt in objectTexts )
+            
+            Mesh createMesh_( string name, string vtx, string face )
             {
-                Debug.Log( $"{txt.obj_name} {txt.vtx} {txt.face}" );
+                //Debug.Log( $"{txt.obj_name} {txt.vtx} {txt.face}" );
+                var vtxs = qVtx.Many().Parse( vtx );
+                var tris = qIdx.Where( x => x.corner_length == 3 ).Select( x => x.indices ).Many().Parse( face );
+                //foreach( var v in vtxs ) Debug.Log( $"{v[ 0 ]} {v[ 1 ]} {v[ 2 ]}" );
+                //foreach( var t in tris ) Debug.Log( $"{t[ 0 ]} {t[ 1 ]} {t[ 2 ]}" );
+
+                var mesh = new Mesh();
+                mesh.name = name;
+                mesh.vertices = vtxs.Select( vcs => new Vector3( vcs[ 0 ], vcs[ 1 ], vcs[ 2 ] ) ).ToArray();
+                mesh.triangles = tris.SelectMany( x => x ).ToArray();
+
+                return mesh;
             }
         }
     }
