f6f89bc493fec9d349c74819ed7ec962a9609b8b
abarabone <abara00@live.jp>
2020-01-27 10:38:11 +0900

    a

--- Assets/Editor/ConvertMqoForMarchingCubes.cs
+++ Assets/Editor/ConvertMqoForMarchingCubes.cs
@@ -119,10 +119,10 @@ public class ConvertMqoForMarchingCubes : MonoBehaviour
             select Convert.ToSingle( negative + num + e )
             ;
         var qVtx =
-            from v0 in qFloatEx.Token()
-            from v1 in qFloatEx.Token()
-            from v2 in qFloatEx.Token()
-            select new Vector3( v0, v1, v2 )
+            from x in qFloatEx.Token()
+            from y in qFloatEx.Token()
+            from z in qFloatEx.Token()
+            select new Vector3( -x/x, y/y, z/z )
             ;
         var qIdx =
             from corner_length in Parse.Decimal.Token().Select( x => int.Parse( x ) )
@@ -169,6 +169,51 @@ public class ConvertMqoForMarchingCubes : MonoBehaviour
         }
     }
 
+
+    struct CubePattarn
+    {
+        public byte primaryId;
+        public byte id;
+        public (sbyte x, sbyte y, sbyte z) dir;
+        public (sbyte x, sbyte y, sbyte z) up;
+        public CubePattarn( byte id )
+        {
+            this.primaryId = id;
+            this.id = id;
+            this.dir = (0, 0, 1);
+            this.up = (0, 1, 0);
+        }
+        public CubePattarn( CubePattarn src, byte id )
+        {
+            this = src;
+            this.id = id;
+        }
+        public CubePattarn RotX()
+        {
+            this.dir = (this.dir.x, this.dir.z, (sbyte)-this.dir.y);
+            this.up  = (this.up.x,  this.up.z,  (sbyte)-this.up.y );
+            return this;
+        }
+        public CubePattarn RotY()
+        {
+            this.dir = ((sbyte)-this.dir.z, this.dir.y, this.dir.x);
+            this.up  = ((sbyte)-this.up.z,  this.up.y,  this.up.x );
+            return this;
+        }
+        public CubePattarn RotZ()
+        {
+            this.dir = (this.dir.y, (sbyte)-this.dir.x, this.dir.z);
+            this.up  = (this.up.y,  (sbyte)-this.up.x,  this.up.z );
+            return this;
+        }
+        public CubePattarn Reverse()
+        {
+            this.dir = ((sbyte)-this.dir.x, (sbyte)-this.dir.y, (sbyte)-this.dir.z);
+            this.up  = ((sbyte)-this.up.x,  (sbyte)-this.up.y,  (sbyte)-this.up.z );
+            return this;
+        }
+    }
+
     static void aaa( (string name, Vector3[] vtxs, int[][] tris)[] objectsData )
     {
 
@@ -198,6 +243,9 @@ public class ConvertMqoForMarchingCubes : MonoBehaviour
             Debug.Log( $"{Convert.ToString(x.cubeId,2).PadLeft(8,'0')}" );
         }
 
+        (sbyte p0, sbyte p1, sbyte p2) toSbyte_( Vector3 vec ) =>
+            ((sbyte)vec.x, (sbyte)vec.y, (sbyte)vec.z);
+
         var vtxsDistinct = qData.SelectMany( x => x.vtxs ).Distinct().ToArray();
         var vtxIdDictByVector3 = vtxsDistinct.Select( ( v, i ) => (v, i) ).ToDictionary( x => x.i, x => x.v );
         Debug.Log( vtxsDistinct.Length );
@@ -208,75 +256,113 @@ public class ConvertMqoForMarchingCubes : MonoBehaviour
                 from tri in cube.tris
                 select new[]
                 {
-                    cube.vtxs[tri[0]],
-                    cube.vtxs[tri[1]],
-                    cube.vtxs[tri[2]],
+                    toSbyte_(cube.vtxs[tri[0]]),
+                    toSbyte_(cube.vtxs[tri[1]]),
+                    toSbyte_(cube.vtxs[tri[2]]),
                 };
         var qStandardId =
             from cube in qData
-            select cube.cubeId
+            select new CubePattarn( cube.cubeId )
             ;
 
-        var qFlipId_x =
-            from id in qStandardId
-            let l = id & 0b_0101_0101
-            let r = id & 0b_1010_1010
-            select (byte)( ( l << 1 ) | ( r >> 1 ) )
+        // 右ねじの回転方向とする
+        IEnumerable<CubePattarn> qRotId_AxisZ_( IEnumerable<CubePattarn> src ) =>
+            from x in src
+            let z0 = x.id & 0b_0000_0101
+            let z1 = x.id & 0b_0101_0000
+            let z2 = x.id & 0b_1010_0000
+            let z3 = x.id & 0b_0000_1010
+            select new CubePattarn( x, (byte)( z0 << 4 | z1 << 1 | z2 >> 4 | z3 >> 1 ) ).RotZ()
+            ;
+        IEnumerable<CubePattarn> qRotId_AxisX_( IEnumerable<CubePattarn> src ) =>
+            from x in src
+            let x0 = x.id & 0b_0000_0011
+            let x1 = x.id & 0b_0011_0000
+            let x2 = x.id & 0b_1100_0000
+            let x3 = x.id & 0b_0000_1100
+            select new CubePattarn( x, (byte)( x0 << 4 | x1 << 2 | x2 >> 4 | x3 >> 2 ) ).RotX()
+            ;
+        IEnumerable<CubePattarn> qRotId_AxisY_( IEnumerable<CubePattarn> src ) =>
+            from x in src
+            let y0 = x.id & 0b_0001_0001
+            let y1 = x.id & 0b_0010_0010
+            let y2 = x.id & 0b_1000_1000
+            let y3 = x.id & 0b_0100_0100
+            select new CubePattarn( x, (byte)( y0 << 1 | y1 << 2 | y2 >> 1 | y3 >> 2 ) ).RotY()
             ;
 
-        var qFlipId_y =
-            from id in qStandardId
-            let u = id & 0b_0000_1111
-            let d = id & 0b_1111_0000
-            select (byte)( ( u << 4 ) | ( d >> 4 ) )
+        //IEnumerable<CubePattarn> qFlipId_X_( IEnumerable<CubePattarn> src ) =>
+        //    from x in src
+        //    let l = x.id & 0b_0101_0101
+        //    let r = x.id & 0b_1010_1010
+        //    select new CubePattarn( x, (byte)( ( l << 1 ) | ( r >> 1 ) ), false, true )
+        //    ;
+        //IEnumerable<CubePattarn> qFlipId_Y_( IEnumerable<CubePattarn> src ) =>
+        //    from x in src
+        //    let u = x.id & 0b_0000_1111
+        //    let d = x.id & 0b_1111_0000
+        //    select new CubePattarn( x, (byte)( ( u << 4 ) | ( d >> 4 ) ), false, true )
+        //    ;
+        //IEnumerable<CubePattarn> qFlipId_Z_( IEnumerable<CubePattarn> src ) =>
+        //    from x in src
+        //    let f = x.id & 0b_0011_0011
+        //    let b = x.id & 0b_1100_1100
+        //    select new CubePattarn( x, (byte)( ( f << 2 ) | ( b >> 2 ) ), false, true )
+        //    ;
+
+        IEnumerable<CubePattarn> qReverseId_( IEnumerable<CubePattarn> src ) =>
+            from x in src
+            select new CubePattarn( x, (byte)( x.id ^ 0b_1111_1111 ) ).Reverse()
             ;
 
-        var qFlipId_z =
-            from id in qStandardId
-            let f = id & 0b_0011_0011
-            let b = id & 0b_1100_1100
-            select (byte)( ( f << 2 ) | ( b >> 2 ) )
-            ;
+        CubePattarn[] rot_( IEnumerable<CubePattarn> src )
+        {
+            var rotx0 = qRotId_AxisX_( src ).ToArray();
+            var rotx1 = qRotId_AxisX_( rotx0 ).ToArray();
+            var rotx2 = qRotId_AxisX_( rotx1 ).ToArray();
+            var rotx = src.Concat( rotx0 ).Concat( rotx1 ).Concat( rotx2 );//.ToArray();
 
-        var qReverseId =
-            from id in qStandardId
-            select (byte)( id ^ 0b_1111_1111 )
-            ;
+            var roty0 = qRotId_AxisY_( rotx ).ToArray();
+            var roty1 = qRotId_AxisY_( roty0 ).ToArray();
+            var roty2 = qRotId_AxisY_( roty1 ).ToArray();
+            var rotxy = rotx.Concat( roty0 ).Concat( roty1 ).Concat( roty2 );//.ToArray();
 
-        var qReverseId_x =
-            from id in qFlipId_x
-            select (byte)( id ^ 0b_1111_1111 )
-            ;
-        var qReverseId_y =
-            from id in qFlipId_y
-            select (byte)( id ^ 0b_1111_1111 )
-            ;
-        var qReverseId_z =
-            from id in qFlipId_z
-            select (byte)( id ^ 0b_1111_1111 )
-            ;
-        //var xflip =
-        //    from cube in standard
-        //    from vtri in cube
-        //    select new[]
-        //    {
-        //        vtri * Vector3.right * -1
-        //    };
+            var rotz0 = qRotId_AxisZ_( rotxy ).ToArray();
+            var rotz1 = qRotId_AxisZ_( rotz0 ).ToArray();
+            var rotz2 = qRotId_AxisZ_( rotz1 ).ToArray();
+            var rotxyz = rotxy.Concat( rotz0 ).Concat( rotz1 ).Concat( rotz2 );//.ToArray();
 
-        var qId = qStandardId
-            .Concat( qFlipId_x )
-            .Concat( qFlipId_y )
-            .Concat( qFlipId_z )
-            .Concat( qReverseId )
-            .Concat( qReverseId_x )
-            .Concat( qReverseId_y )
-            .Concat( qReverseId_z )
+            return rotxyz.ToArray();
+        }
+
+        var standardId = qStandardId.ToArray();//.Concat( Enumerable.Repeat((byte)0, 1) ).ToArray();
+
+        var stds = standardId;
+        //var flips = qFlipId_X_( standardId );//.ToArray();
+        var revs = qReverseId_( standardId );//.ToArray();
+        var rotstds = rot_( stds );
+        //var rotFlips = rot_( flips );
+        var rotrevs = rot_( revs );
+        var qId =
+            from x in rotstds.Concat( rotrevs )//.Concat( rotFlips )
+            group x by (x.id, x.primaryId) into g
+            orderby g.Key
+            select g
             ;
+        var idGroups = qId.ToArray();
 
         using( var f = new StreamWriter( @"C:\Users\abarabone\Desktop\mydata\mc.txt" ) )
         {
-            var s = string.Join( "\r\n", qId.Select( x => Convert.ToString( x, 2 ).PadLeft(8,'0') ) );
-            f.Write( s );
+            f.WriteLine( idGroups.Length );
+            var ss =
+                from g in idGroups
+                from p in g
+                let id = Convert.ToString( p.id, 2 ).PadLeft( 8, '0' )
+                let primaryId = Convert.ToString( p.primaryId, 2 ).PadLeft( 8, '0' )
+                select (id, primaryId, p.dir, p.up)
+                ;
+            var s = string.Join( "\r\n", ss );
+            f.WriteLine( s );
         }
 
         var expandPattern256 = qData
