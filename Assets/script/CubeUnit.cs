f15f6185b73b46532c12487c3e5c60cf45ac80be
abarabone <abara00@live.jp>
2020-02-01 22:05:17 +0900

    a

--- Assets/script/CubeUnit.cs
+++ Assets/script/CubeUnit.cs
@@ -16,12 +16,49 @@ namespace mc
     {
         public byte unit;
 
-        public uint this[int x, int y, int z]
+        public uint this[ int x, int y, int z ]
         {
             get => (byte)( ( this.unit >> ( x + y << 2 + z << 1 ) ) & 1 );
-            set => this.unit ^= (byte)( (value << ( x + y << 2 + z << 1 )) ^ this.unit );
+            set 
+            { 
+                var shift = ( x + ( y << 2 ) + ( z << 1 ) );
+                this.unit ^= (byte)( ( value << shift ) ^ ( this.unit & ( 1 << shift ) ) );
+            }
         }
     }
+    [StructLayout( LayoutKind.Explicit )]
+    public struct Cube2x2
+    {
+        [FieldOffset( 0 )]
+        public uint unit2x2;
 
+        [FieldOffset( 0 )] CubeUnit unit0;
+        [FieldOffset( 1 )] CubeUnit unit1;
+        [FieldOffset( 2 )] CubeUnit unit2;
+        [FieldOffset( 3 )] CubeUnit unit3;
+        
 
+        //public uint this[ int x, int y, int z ]
+        //{
+        //    //get => (byte)( 
+        //    //set
+        //    //{
+        //    //    var shift = ( x + ( y << 2 ) + ( z << 1 ) );
+        //    //    this.unit ^= (byte)( ( value << shift ) ^ ( this.unit & ( 1 << shift ) ) );
+        //    //}
+        //}
+    }
+
+    // x 0101 0101
+    // y 1111 0000
+    // z 1100 1100
+    public struct Cubes
+    {
+        uint[] units;
+
+        public this[int x, int y, int z]
+        {
+
+        }
+    }
 }
