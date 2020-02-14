a97f1ceba831e3a64a25ec0c382e4325d3d7ea23
abarabone <abara00@live.jp>
2020-02-14 21:39:13 +0900

    a

--- Assets/script/CubeUnit.cs
+++ Assets/script/CubeUnit.cs
@@ -9,63 +9,7 @@ using Unity.Mathematics;
 
 namespace mc
 {
-
-
-
-    public struct CubeUnit
-    {
-        public byte unit;
-
-        public uint this[ int x, int y, int z ]
-        {
-            get => (byte)( ( this.unit >> ( x + y << 2 + z << 1 ) ) & 1 );
-            set 
-            { 
-                var shift = ( x + ( y << 2 ) + ( z << 1 ) );
-                this.unit ^= (byte)( ( value << shift ) ^ ( this.unit & ( 1 << shift ) ) );
-            }
-        }
-    }
-    [StructLayout( LayoutKind.Explicit )]
-    public struct Cube2x2
-    {
-        [FieldOffset( 0 )]
-        public uint unit2x2;
-
-        [FieldOffset( 0 )] CubeUnit unit0;
-        [FieldOffset( 1 )] CubeUnit unit1;
-        [FieldOffset( 2 )] CubeUnit unit2;
-        [FieldOffset( 3 )] CubeUnit unit3;
-        [FieldOffset( 4 )] CubeUnit unit4;
-        [FieldOffset( 5 )] CubeUnit unit5;
-        [FieldOffset( 6 )] CubeUnit unit6;
-        [FieldOffset( 7 )] CubeUnit unit7;
-
-        //public uint this[ int x, int y, int z ]
-        //{
-        //    //get => (byte)( 
-        //    //set
-        //    //{
-        //    //    var shift = ( x + ( y << 2 ) + ( z << 1 ) );
-        //    //    this.unit ^= (byte)( ( value << shift ) ^ ( this.unit & ( 1 << shift ) ) );
-        //    //}
-        //}
-    }
-
-    // x 0101 0101
-    // y 1111 0000
-    // z 1100 1100
-    public struct Cubes
-    {
-        uint[] units;
-
-        //public this[int x, int y, int z]
-        //{
-
-        //}
-    }
-
-
+    
     public class CubeGrid
     {
         public uint xLength, zLength, yLength;
---
+++ Assets/script/GridArray.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Unity.Mathematics;
+
+namespace mc
+{
+
+    public class GridArray : MonoBehaviour
+    {
+
+        [SerializeField]
+        public float3 GridLength;
+
+        CubeGrid[] grids;
+
+        
+        void Awake()
+        {
+            var range = this.GridLength + 2;
+            var totalLength = (int)math.dot( this.GridLength, Vector3.one );
+            this.grids = new CubeGrid[totalLength];
+        }
+        
+        void Update()
+        {
+
+        }
+    }
+
+}
+
