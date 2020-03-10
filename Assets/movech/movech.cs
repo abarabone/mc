c1f148918de6a54a20db7e8ca82a0a89973e9fdf
abarabone <abara00@live.jp>
2020-03-10 22:10:25 +0900

    a

---
+++ Assets/movech/movech.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movech : MonoBehaviour
+{
+
+    Camera cam;
+    Rigidbody rb;
+
+    Vector3 prepos;
+    
+    void Start()
+    {
+        this.cam = this.GetComponentInChildren<Camera>();
+        this.rb = this.GetComponentInChildren<Rigidbody>();
+        
+    }
+    
+    void Update()
+    {
+        var nowpos = Input.mousePosition;
+
+        var d = ( nowpos - prepos ) * Time.deltaTime * 30.0f;
+
+        var e = this.rb.rotation.eulerAngles;
+        e.y += d.x;
+        this.rb.rotation = Quaternion.Euler( e );
+
+        var tf = this.transform;
+        cam.transform.RotateAround( tf.position, tf.right, -d.y );
+
+        if( Input.GetKey( KeyCode.Space ) )
+            rb.AddForce( Vector3.up * 100.0f );
+
+        this.prepos = nowpos;
+    }
+}
